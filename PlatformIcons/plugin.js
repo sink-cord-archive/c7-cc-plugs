da208ac36de0a75b5fd8cc356fe41506a526bea4
Cynthia Foxwell <gamers@riseup.net>
2021-11-21 14:05:55 -0700

    PlatformIcons: add to dm list and user popout

--- PlatformIcons/plugin.js
+++ PlatformIcons/plugin.js
@@ -1,20 +1,38 @@
-import {findByDisplayName} from "@cumcord/modules/webpackModules";
-import {after} from "@cumcord/patcher";
+import {
+  find,
+  findByDisplayName,
+  findByProps,
+} from "@cumcord/modules/webpackModules";
+import {findInReactTree} from "@cumcord/utils";
+import {before, after} from "@cumcord/patcher";
 import {React} from "@cumcord/modules/common";
 
 import IconsWrapper from "./IconsWrapper";
 
 const MemberListItem = findByDisplayName("MemberListItem");
+const PrivateChannel = find(
+  (x) =>
+    x &&
+    x.default &&
+    x.default.render &&
+    typeof x.default.render == "function" &&
+    x.default.render
+      .toString()
+      .match(/{className:.\.default\.nameAndDecorators}/)
+);
+const UserPopoutComponents = findByProps("UserPopoutInfo");
 
-let unpatch;
+const DMChannelStore = findByProps("getDMUserIds");
+const UserStore = findByProps("getUser", "getCurrentUser");
+
+const patches = [];
 
 export default (data) => {
   return {
     onLoad() {
-      unpatch = after(
-        "renderDecorators",
-        MemberListItem.prototype,
-        (_, ret) => {
+      // member list
+      patches.push(
+        after("renderDecorators", MemberListItem.prototype, (_, ret) => {
           const {props} = ret._owner.stateNode;
           ret.props.children.splice(
             0,
@@ -23,11 +41,61 @@ export default (data) => {
               user: props.user,
             })
           );
-        }
+        })
+      );
+
+      // dm list
+      patches.push(
+        before("render", PrivateChannel.default, ([props]) => {
+          if (props.to && typeof props.to == "string") {
+            const channelId = props.to.match(/@me\/(.+?)$/)[1];
+            if (channelId) {
+              const channel = DMChannelStore.getChannel(channelId);
+              if (channel?.type == 1) {
+                const userId = channel.recipients[0];
+                const user = UserStore.getUser(userId);
+
+                if (user) {
+                  if (!Array.isArray(props.decorators)) {
+                    props.decorators = [props.decorators];
+                  }
+
+                  props.decorators.push(
+                    React.createElement(IconsWrapper, {
+                      user,
+                    })
+                  );
+                }
+              }
+            }
+          }
+        })
+      );
+
+      // user popout
+      patches.push(
+        after("UserPopoutInfo", UserPopoutComponents, (_, ret) => {
+          const headerText = findInReactTree(
+            ret,
+            (x) => x.className && x.className.startsWith("headerText-")
+          );
+          if (headerText) {
+            const nameTag = headerText.children[1];
+            headerText.children[1] = React.createElement(
+              "div",
+              {style: {display: "flex", flexDirection: "row"}},
+              nameTag,
+              React.createElement(IconsWrapper, {user: nameTag.props.user})
+            );
+          }
+        })
       );
     },
     onUnload() {
-      unpatch();
+      for (const unpatch of patches) {
+        unpatch();
+      }
+      patches.splice(0, patches.length);
     },
   };
 };
